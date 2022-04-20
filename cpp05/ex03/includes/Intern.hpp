e51f11e188fe2a929cdc83a1b5642583be7e7dd3
Louis CALVIE <lcalvie@e2r10p19.clusters.42paris.fr>
2022-04-20 12:45:22 +0200

    aerb ourn

---
+++ cpp05/ex03/includes/Intern.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 12:41:19 by lcalvie           #+#    #+#             */
+/*   Updated: 2022/04/20 12:42:31 by lcalvie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+class Intern
+{
+        public:
+                Intern();
+                Intern(Intern const& copy);
+                ~Intern();
+                Intern &operator=(Intern const& copy);
+
+        private :
+
+};
+
+
+#endif
---
+++ cpp05/ex03/srcs/Intern.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 12:42:39 by lcalvie           #+#    #+#             */
+/*   Updated: 2022/04/20 12:44:24 by lcalvie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Intern.hpp"
+
+Intern::Intern()
+{
+}
+
+Intern::Intern(Intern const& copy)
+{
+}
+
+Intern::~Intern()
+{
+}
+
+Intern &Intern::operator=(Intern const& copy)
+{
+
+        return *this;
+}
