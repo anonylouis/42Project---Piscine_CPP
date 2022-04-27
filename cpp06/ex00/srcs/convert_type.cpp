7681ba2dd2f2e8485c0bb231ae110bed7d8cab07
Louis CALVIE <lcalvie@e2r10p15.clusters.42paris.fr>
2022-04-27 18:08:41 +0200

    merci mababou:

--- cpp06/ex00/srcs/convert_type.cpp
+++ cpp06/ex00/srcs/convert_type.cpp
@@ -6,11 +6,20 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 18:43:59 by lcalvie           #+#    #+#             */
-/*   Updated: 2022/04/26 03:29:49 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/04/27 17:20:00 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "convert.hpp"
+
+void display_char(int i)
+{
+	char c = static_cast<char>(i);
+	if (!isprint(c))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << c << std::endl;
+}
 
 void    convert_char(std::string const& str)
 {
@@ -20,7 +29,7 @@ void    convert_char(std::string const& str)
     double d = static_cast<double>(c);
 
     std::cout << "char: ";
-    if (!isprint(i))
+    if (!isprint(c))
        std::cout << "Non displayable" << std::endl;
     else
        std::cout << c << std::endl;
@@ -31,7 +40,7 @@ void    convert_char(std::string const& str)
 
 void    convert_int(std::string const& str)
 {
-	double d = std::strtod(str.c_str(), nullptr);
+	double d = std::strtod(str.c_str(), 0);
 	if (d == HUGE_VAL || d == -HUGE_VAL || d < std::numeric_limits<int>::min() || d > std::numeric_limits<int>::max())
 	{
 		std::cout << "char: impossible" << std::endl;
@@ -42,12 +51,18 @@ void    convert_int(std::string const& str)
 	}
 	float f = static_cast<float>(d);
 	int i = static_cast<int>(d);
-
+	
 	std::cout << "char: ";
-	if (!isprint(i))
-	std::cout << "Non displayable" << std::endl;
+	if (i >= std::numeric_limits<char>::min() && i <= std::numeric_limits<char>::max())
+	{
+		char c = static_cast<char>(i);
+		if (!isprint(c))
+			std::cout << "Non displayable" << std::endl;
+		else
+			std::cout << c << std::endl;
+	}
 	else
-	std::cout << static_cast<char>(i) << std::endl;
+		std::cout << "impossible" << std::endl;
 	std::cout << "int: " << i << std::endl;
 	std::cout << "float: " << f << std::endl;
 	std::cout << "double: " << d << std::endl;
@@ -55,7 +70,7 @@ void    convert_int(std::string const& str)
 
 void    convert_float(std::string const& str)
 {
-	double d = std::strtod(str.c_str(), nullptr);
+	double d = std::strtod(str.c_str(), 0);
 	if (d == HUGE_VAL || d == -HUGE_VAL || d < std::numeric_limits<float>::min() || d > std::numeric_limits<float>::max())
 	{
 		
@@ -75,7 +90,7 @@ void    convert_float(std::string const& str)
 
 void    convert_double(std::string const& str)
 {
-	double d = std::strtod(str.c_str(), nullptr);
+	double d = std::strtod(str.c_str(), 0);
 	float f = static_cast<float>(d);
 	int i = static_cast<int>(d);
 
---
+++ cpp06/ex00/test.sh
@@ -0,0 +1,6 @@
+
+echo "CONVERT 49 :"
+./convert 49
+echo ""
+echo "CONVERT INT MAX :"
+./convert 2147483647
